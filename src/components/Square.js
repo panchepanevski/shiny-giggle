43cb8f6cc9b6fd80891a0008c4d45ece47383d73
panchepanevski <pance.panevski@gmail.com>
2019-11-20 15:30:50 +0100

    global styles

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,18 +2,13 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const Button = styled.button`
-  background: #f2f2f2;
-  border: 1px solid #999;
+  border: 3px solid #292929;
   float: left;
-  font-size: 24px;
+  font-size: 40px;
   font-weight: bold;
-  line-height: 50px;
-  height: 33.3%;
-  margin-right: -1px;
-  margin-top: -1px;
+
   padding: 0;
   text-align: center;
-  width: 33.3%;
 
   :focus {
     outline: none;
